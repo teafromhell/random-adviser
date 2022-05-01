d124e2cbb3e762d6a7a4c05695caa88a88109667
Dmitriy <tefromhell@gmail.com>
2022-05-01 20:25:51 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import dice from "./images/icon-dice.svg";
 
 function App() {
   const [post, setPost] = useState("");
+  console.log(1)
   useEffect(() => {
+    getPost()
     setPost(JSON.parse(localStorage.getItem("advice")));
   }, []);
 
   async function getPost() {
-    const response = await axios.get(`https://api.adviceslip.com/advice`);
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    console.log(response)
     setPost(response.data.slip);
     localStorage.setItem("advice", JSON.stringify(response.data.slip));
   }
-
   return (
     <div className="app">
       <h5 className="app__header">ADVICE #{post.id}</h5>
@@ -25,7 +27,7 @@ function App() {
       <div className="app__divider">
         <img src={dividerDesktop} alt="divider" style={{ width: 300 }} />
       </div>
-      <div className="app__dice" onClick={getPost}>
+      <div className="app__dice" onClick={()=>getPost()}>
         <img src={dice} alt="dice" style={{ width: 20, height: 20 }} />
       </div>
     </div>
